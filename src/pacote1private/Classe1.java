244c9fc24b5ce2bf78f6924e8e410ce3108db027
fsvdavi <d.silva.viana.red@gmail.com>
2025-01-03 22:22:44 -0300

    Pondo atributos, getters e setter na classe1 private

---
+++ src/pacote1private/Classe1.java
@@ -0,0 +1,32 @@
+package pacote1private;
+
+public class Classe1 {
+	private String atributo1;
+    private int atributo2;
+    private double atributo3;
+
+    public String getAtributo1() {
+        return atributo1;
+    }
+    public void setAtributo1(String atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    
+    public int getAtributo2() {
+        return atributo2;
+    }
+    public void setAtributo2(int atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    
+    public double getAtributo3() {
+        return atributo3;
+    }
+    public void setAtributo3(double atributo3) {
+        this.atributo3 = atributo3;
+    }
+}
+
+
